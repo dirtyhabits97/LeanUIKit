2660b0386a237090291bc5c99f8e57f10795358f
Gonzalo Reyes Huertas <gonzalorehu.97.31@gmail.com>
2018-10-27 13:32:01 -0500

    Initial commit

---
+++ LeanUIKit/LeanUIKit.h
@@ -0,0 +1,19 @@
+//
+//  LeanUIKit.h
+//  LeanUIKit
+//
+//  Created by Gonzalo Reyes Huertas on 10/27/18.
+//  Copyright © 2018 Gonzalo Reyes Huertas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeanUIKit.
+FOUNDATION_EXPORT double LeanUIKitVersionNumber;
+
+//! Project version string for LeanUIKit.
+FOUNDATION_EXPORT const unsigned char LeanUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeanUIKit/PublicHeader.h>
+
+
