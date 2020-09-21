12aff099a185566511cb92713e413b83f548156d
Gozalo Reyes Huertas <gonzalo.reyes@scotiabank.com.pe>
2020-09-21 17:16:13 -0500

    WIP(Documentation): Added documentation for UIButton extensions.

--- Source/UIButton+Extensions.swift
+++ Source/UIButton+Extensions.swift
@@ -10,16 +10,17 @@ import UIKit
 
 public extension UIButton {
     
+    /// Proxy property for the button's title.
     var title: String? {
         get { return title(for: .normal) }
         set { setTitle(newValue, for: .normal) }
     }
-    
+    /// Proxy property for the button's attributed title.
     var attributedTitle: NSAttributedString? {
         get { return attributedTitle(for: .normal) }
         set { setAttributedTitle(newValue, for: .normal) }
     }
-    
+    /// Proxy property for the button's title color.
     var titleColor: UIColor? {
         get { return titleColor(for: .normal) }
         set { setTitleColor(newValue, for: .normal) }
@@ -29,6 +30,12 @@ public extension UIButton {
 
 public extension UIButton {
     
+    /**
+     Creates a `UIButton` with rounded corners.
+     
+     - Parameters:
+        - cornerRadius: the corner radius to use for the rounded corners.
+     */
     static func roundedButton(cornerRadius: CGFloat) -> UIButton {
         let button = UIButton()
         button.layer.cornerRadius = cornerRadius
