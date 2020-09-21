837641da4c4b078e7f1877744b3696528aa38ab2
Gozalo Reyes Huertas <gonzalo.reyes@scotiabank.com.pe>
2020-09-21 17:17:47 -0500

    WIP(Documentation): Added documentation for UIImage extensions.

--- Source/UIImage+Extensions.swift
+++ Source/UIImage+Extensions.swift
@@ -10,6 +10,13 @@ import UIKit
 
 public extension UIImage {
     
+    /**
+     Returns an `UIImage` with the given `alpha`.
+     
+     - Parameters:
+        - alpha: the alpha to apply to the copy of
+                 the current `UIImage`.
+     */
     func applying(alpha: CGFloat) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(size, false, scale)
         draw(at: .zero, blendMode: .normal, alpha: alpha)
