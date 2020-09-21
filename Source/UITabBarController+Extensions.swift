947c0f76a6ce6c0207bb42ca65fbc12477eeb301
Gozalo Reyes Huertas <gonzalo.reyes@scotiabank.com.pe>
2020-09-21 17:24:28 -0500

    WIP(Documentation): Added documentation for UITabBarController extensions.

--- Source/UITabBarController+Extensions.swift
+++ Source/UITabBarController+Extensions.swift
@@ -10,6 +10,13 @@ import UIKit
 
 public extension UITabBarController {
     
+    /**
+     Returns the index of the first `UIViewController` of the given type
+     in the `UITabBarController.viewControllers` stack.
+     
+     - Parameters:
+        - viewControllerType: the type of viewcontroller to look up.
+     */
     func index<V: UIViewController>(for viewControllerType: V.Type) -> Int? {
         guard let viewControllers = self.viewControllers else { return nil }
         for index in viewControllers.indices {
